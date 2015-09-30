c0ca5399d48a3ec0b7b680fc499ea21377603e0b
maxthauer <max@maxthauer.com>
2015-09-30 17:04:04 -0400

    Formatting Changes
    Files now printed in reverse alphabetical order on separate lines. Also
    adjusted for Python 3.

--- email-directory-listing.py
+++ email-directory-listing.py
@@ -1,42 +1,49 @@
-#Python 2.7.6
-# Import smtplib for the actual sending function
-import smtplib
-import os, sys
-# Import the email modules we'll need
+#!/usr/bin/python3
+'''
+Get the list of a directory's contents in an email.
+Useful to verify if a backup completed successfully.
+For example: if you see new files in the email, you know that
+the backup was completed. Files listed in reverse alphabetical order
+to accomodate filenames that represent a timestamp.
 
-path = "/path/to/your/directory"
-dirs = os.listdir( path )
+*Uses Gmail to send the email message. You'll need valid Gmail credentials.
+
+By Max Thauer
+'''
+import smtplib, os, sys 
+
+gmailUsername = ("USERNAME@gmail.com")
+gmailPassword = ("PASSWORD")
+senderName = ("SENDER_NAME_HERE")
+receiverName =("RECEIVER_NAME_HERE")
+sender = ("USERNAME@gmail.com")
+receivers = ['RECEIVER_ADDRESSES@domain.tld']
+mimeVersion = ("1.0")
+contentType = ("text/plain")
+subject = ("SUBJECT_GOES_HERE")
+
+path = ("/path/to/your/directory/")
+dirs = os.listdir(path)
+
+fileList = []
+recipients = ''.join(receivers)
 
 for file in dirs:
-	mylist = dirs 
-	for elem in mylist:
-		print "\n".join(mylist)
-	
-#need to alter the contents of mylist to have new lines. 
-# eventually add .reverse() to reverse order of list for newest files first
+    fileList.append("{}\n".format(file))
 
+fileList.reverse()
+body = "".join(fileList)
 
-try:
-    sender = 'SENDER-EMAIL@gmail.com'
-    receivers = ['username@domain.tld']
-    message = """From: Daily Update <SENDER-EMAIL@gmail.com>
-To: YOUR NAME <username@domain.tldm>
-MIME-Version: 1.0
-Content-type: text/html
-Subject: Daily Photos Backup Directory Listing
+message = "From: "+senderName+" <"+sender+">\nTo: "+receiverName+" <"+recipients+">\nMime version: "+mimeVersion+"\nContent-type: "+contentType+"\nSubject: "+subject+"\n\n{}".format(body)
 
-<h3>Directory Listing:</h3>
-%s
-	"""%mylist
-	
+try: 
     smtpObj = smtplib.SMTP(host='smtp.gmail.com', port=587)
     smtpObj.ehlo()
     smtpObj.starttls()
     smtpObj.ehlo()
-    smtpObj.login('SENDER-EMAIL@gmail.com','PASSWORD')
+    smtpObj.login(gmailUsername,gmailPassword)
     smtpObj.sendmail(sender, receivers, message)
     smtpObj.quit()
-    print "Successfully sent email"
-except smtplib.SMTPException,error:
-    print str(error)
-    print "Error: unable to send email"
+    print ("Successfully sent email")
+except smtplib.SMTPException:
+    print ("Error: unable to send email")
